df75a6e3a5093f760e6971d459973235607a237f
leonardo <vieiraleo10@hotmail.com>
2024-12-29 15:14:57 -0300

    Alteração no comportamento do background-color da lista do menu hambúrgeur ao clicar no menu hambúrguer pelo arquivo js

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 // Função que altera o valor da propriedade display
-function clickMenu() {
-  if (itens.style.display == 'block') {
-    itens.style.display = 'none'
-  } else {
-    itens.style.display = 'block'
-  }
-};
+// function clickMenu() {
+//   if (itens.style.display == 'block') {
+//     itens.style.display = 'none'
+//   } else {
+//     itens.style.display = 'block'
+//   }
+// };
 
-document.querySelector('#burguer').onclick = clickMenu;
+// document.querySelector('#burguer').onclick = clickMenu;
+
+
+
+
+/* CAPTURANDO ELEMENTOS NO DOM */ 
+const imgMenuHamburguer = document.getElementById('burguer');
+const menuListaMenuHamburguer = document.getElementById('itens');
+
+/* O QUE ACONTECE AO CLICAR NO MENU HAMBÚRGUER */ 
+imgMenuHamburguer.addEventListener('click', alterarDisplay);
+
+/* DEFINIÇÃO DA FUNÇÃO */
+function alterarDisplay(evento) {
+  evento.preventDefault();
+
+  if (menuListaMenuHamburguer.style.display == 'block') {
+    menuListaMenuHamburguer.style.display = 'none';
+    imgMenuHamburguer.style.backgroundColor = 'var(--azul-celeste)';
+  }
+  else {
+    menuListaMenuHamburguer.style.display = 'block';
+    imgMenuHamburguer.style.backgroundColor = 'var(--cor-de-fundo)';
+    }
+
+}
