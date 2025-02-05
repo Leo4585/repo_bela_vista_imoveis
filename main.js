6ecd1d6ca7c1965a975cf7dd3b5b768db484a8c6
leonardo <vieiraleo10@hotmail.com>
2025-02-05 20:38:29 -0300

    Alterações diversas na função que altera o display do menu hambúrguer

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import { alterarDisplayMenuHamburguer } from "./scripts/alterarDisplayMenuHamburguer.js";
 const imgMenuHamburguer = document.getElementById('burguer');
+const menuListaMenuHamburguer = document.getElementById('itens');
+
 
 
 document.addEventListener('click', (evento) => {
   // O QUE ACONTECE AO CLICAR NO MENU HAMBÚRGUER
-  if(evento.target.id == imgMenuHamburguer.id) {
+  if (evento.target.id == imgMenuHamburguer.id) {
+    console.log(evento.target.id)
     alterarDisplayMenuHamburguer();
   }
+
 })
 
--- scripts/alterarDisplayMenuHamburguer.js
+++ scripts/alterarDisplayMenuHamburguer.js
@@ -2,17 +2,9 @@
 const imgMenuHamburguer = document.getElementById('burguer');
 const menuListaMenuHamburguer = document.getElementById('itens');
 
-export function alterarDisplayMenuHamburguer(evento) {
+export function alterarDisplayMenuHamburguer() {
 // DEFINIÇÃO DA FUNÇÃO
   //evento.preventDefault();   // Previne o recarregamento do formulário (evento padrão)
-  
-  if (menuListaMenuHamburguer.style.display == 'block') {
-    menuListaMenuHamburguer.style.display = 'none';
-    imgMenuHamburguer.style.backgroundColor = 'var(--azul-celeste)';
-  }
-  else {
-    menuListaMenuHamburguer.style.display = 'block';
-    imgMenuHamburguer.style.backgroundColor = 'var(--cor-de-fundo)';
-    }
-
+  imgMenuHamburguer.classList.toggle('cabecalho__menu__hamburguer');
+  menuListaMenuHamburguer.classList.toggle('itens__menu-hamburguer');
 }
