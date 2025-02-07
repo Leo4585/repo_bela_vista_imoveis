9b780adc966ee0f875272d1b1cf73bfd4a809318
leonardo <vieiraleo10@hotmail.com>
2025-02-07 06:42:24 -0300

    Correção do comportamento da apresentação da lista do menu hambúrguer ao clicar no menu hambúrguer ou em outra parte do documento que não seja o menu hambúrguer

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-import { alterarDisplayMenuHamburguer } from "./scripts/alterarDisplayMenuHamburguer.js";
+import { alternarDisplayMenuHamburguer } from "./scripts/alterarDisplayMenuHamburguer.js";
 const imgMenuHamburguer = document.getElementById('burguer');
 const menuListaMenuHamburguer = document.getElementById('itens');
 
 
-
-document.addEventListener('click', (evento) => {
-  // O QUE ACONTECE AO CLICAR NO MENU HAMBÚRGUER
+// O que acontece ao clicar na img do menu hambúrguer
+imgMenuHamburguer.addEventListener('click', (evento) => {
   if (evento.target.id == imgMenuHamburguer.id) {
     console.log(evento.target.id)
-    alterarDisplayMenuHamburguer();
+    alternarDisplayMenuHamburguer();
   }
+});
 
-})
+// Quando o clique não é na lista nem na img do menu hambúrguer
+document.addEventListener('click', (evento) => {
+  if (!menuListaMenuHamburguer.contains(evento.target) && !imgMenuHamburguer.contains(evento.target)) {
+    menuListaMenuHamburguer.classList.add('itens__menu-hamburguer');
+    imgMenuHamburguer.classList.add('cabecalho__menu__hamburguer');
+  }
+});
 
--- scripts/alterarDisplayMenuHamburguer.js
+++ scripts/alterarDisplayMenuHamburguer.js
@@ -2,7 +2,7 @@
 const imgMenuHamburguer = document.getElementById('burguer');
 const menuListaMenuHamburguer = document.getElementById('itens');
 
-export function alterarDisplayMenuHamburguer() {
+export function alternarDisplayMenuHamburguer() {
 // DEFINIÇÃO DA FUNÇÃO
   //evento.preventDefault();   // Previne o recarregamento do formulário (evento padrão)
   imgMenuHamburguer.classList.toggle('cabecalho__menu__hamburguer');
