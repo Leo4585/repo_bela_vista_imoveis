44fef57ddb7ad174f0befd0f242a02569cf5b1bc
leonardo <vieiraleo10@hotmail.com>
2025-02-19 19:41:43 -0300

    Inicialização do carrossel após o código html

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,107 @@
 import { alternarDisplayMenuHamburguer } from "./alterarDisplayMenuHamburguer.js";
 const imgMenuHamburguer = document.getElementById('burguer');
 const menuListaMenuHamburguer = document.getElementById('itens');
+let imoveis = [];
+const endpointDaAPI = "https://leo4585.github.io/imoveis.json";
+const elementoParaInserirImoveis = document.getElementById('imoveis');
 
+getBuscarImoveisDaAPI();
+
+async function getBuscarImoveisDaAPI() {
+  const res = await fetch(endpointDaAPI);
+  imoveis = await res.json();
+  console.table(imoveis);
+  exibirImoveisNaTela(imoveis)
+}
+
+function exibirImoveisNaTela(listaDeImoveis) {
+  listaDeImoveis.forEach(imovel => {
+    elementoParaInserirImoveis.innerHTML += `
+      <article class="anuncios-em-destaque__article">
+                <figure>
+                    <div class="anuncios-em-destaque__figure__div">
+                        <!-- Slider main container -->
+                        <div class="swiper">
+                            <!-- Additional required wrapper -->
+                            <div class="swiper-wrapper">
+                                <!-- Slides -->
+                                <div class="swiper-slide"> <img
+                                        class="swiper-slide__imagem"
+                                        src="img/casa1-imagem6.jpg" alt="imagem do imóvel ou terreno"></div>
+                                <div class="swiper-slide"> <img
+                                        class="swiper-slide__imagem"
+                                        src="img/casa1-imagem3.jpg" alt="imagem do imóvel ou terreno"></div>
+                                <div class="swiper-slide"> <img
+                                        class="swiper-slide__imagem"
+                                        src="img/casa1-imagem2.jpg" alt="imagem do imóvel ou terreno"></div>
+                                <div class="swiper-slide"> <img
+                                        class="swiper-slide__imagem"
+                                        src="img/casa1-imagem7.jpg" alt="imagem do imóvel ou terreno"></div>
+                                <div class="swiper-slide"> <img
+                                        class="swiper-slide__imagem"
+                                        src="img/casa1-imagem8.jpg" alt="imagem do imóvel ou terreno"></div>
+                                ...
+                            </div>
+                            <!-- If we need pagination -->
+                            <div class="swiper-pagination"></div>
+
+                            <!-- If we need navigation buttons -->
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-button-next"></div>
+                        </div>
+                    </div>
+
+                    <figcaption class="anuncios-em-destaque__figcaption__legendas">
+                        <div class="caixa__legenda__titulo">
+                            <h3 class="legenda__titulo">Casa à venda</h3>
+                            <h3 class="legenda__titulo">R$ 210 mil</h3>
+                        </div>
+                        <div>
+                            <p class="legenda__nome-do-bairro">Residencial Jardim Universitário 1</p>
+                            <p class="legenda__nome-da-rua">Rua T, 49</p>
+                            <p class="legenda__nome-da-cidade">Irecê, Bahia</p>
+                        </div>
+
+                        <div class="container__icones-de-informacoes">
+                            <div class="icones-de-informacoes__box">
+                                <img class="icones-de-informacoes__box__img" src="img/icone_metragem.svg" alt="Área construída">
+                                <p>120m²</p>
+                            </div>
+                            <div class="icones-de-informacoes__box">
+                                <img class="icones-de-informacoes__box__img" src="img/icone_dormitorio.svg" alt="Dormitórios">
+                                <p>3</p>
+                            </div>
+                            <div class="icones-de-informacoes__box">
+                                <img class="icones-de-informacoes__box__img" src="img/icone_banheiro.svg" alt="Banheiros">
+                                <p>2</p>
+                            </div>
+                            <div class="icones-de-informacoes__box">
+                                <img class="icones-de-informacoes__box__img" src="img/icone_garagem.svg" alt="Vagas">
+                                <p>1</p>
+                            </div>
+                        </div>
+                    </figcaption>
+                </figure>
+            </article>
+    `
+  });
+  // Inicialização do Swiper (que deve ocorrer após o código HTML)
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+  });
+
+}
 
 // O que acontece ao clicar na img do menu hambúrguer
 imgMenuHamburguer.addEventListener('click', (evento) => {
