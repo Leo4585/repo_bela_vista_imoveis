1071e721895654d2e02fe0a2c084b33faaca09a4
leonardo <vieiraleo10@hotmail.com>
2025-03-12 17:41:31 -0300

    Alterações

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ejs from 'ejs';
 
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
 app.route('/').get((req, res) => {
-  res.send('Rota da página inicial chamada!');
+  res.render('index');
 });
 
 export default app;
