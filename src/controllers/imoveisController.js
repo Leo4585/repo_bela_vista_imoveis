fc9dec09b936750052580e62bf1117337f3e6b8e
leonardo <vieiraleo10@hotmail.com>
2025-03-13 07:43:32 -0300

    Criação de rotas e controllers

--- src/controllers/imoveisController.js
+++ src/controllers/imoveisController.js
@@ -1,7 +1,27 @@
 class ImoveisController {
   static async paginaInicial (req, res) {
-    res.render('index');
-  }
+    try {
+      res.render('index');    
+    } catch (error) {
+      res.send('Erro ao carregar a página inicial');
+    }
+  };
+
+  static async contato (req, res) {
+    try {
+      res.render('contato');
+    } catch (error) {
+      res.send('Erro ao carregar a página contato');
+    }
+  };
+
+  static async quemSomos (req, res) {
+    try {
+      res.render('quemSomos');
+    } catch (error) {
+      res.send('Erro ao carregar a página Quem Somos');
+    }
+  };
 
 };
 
--- src/routes/imoveisRoutes.js
+++ src/routes/imoveisRoutes.js
@@ -5,5 +5,7 @@ import ImoveisController from '../controllers/imoveisController.js';
 const routes = express.Router(); // Const que tem todas as rotas
 
 routes.get('/', ImoveisController.paginaInicial);
+routes.get('/quemsomos', ImoveisController.quemSomos);
+routes.get('/contato', ImoveisController.contato);
 
 export default routes;
