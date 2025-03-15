1217f757cec8ee1535405f24f0cf1c2c0961315e
leonardo <vieiraleo10@hotmail.com>
2025-03-15 16:31:36 -0300

    Exclusão de algumas linhas de comentários

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,10 @@
-// import { alternarDisplayMenuHamburguer } from "./alterarDisplayMenuHamburguer.js";
-// import { exibirImoveisNaTela } from "./exibirImoveisNaTela.js";
-// import { filtrarImoveis } from "./filtrarImoveis.js";
+import { alternarDisplayMenuHamburguer } from "./alterarDisplayMenuHamburguer.js";
 
 
-// export let imoveis = [];
-// const endpointDaAPI = "https://leo4585.github.io/imoveis.json";
-// getBuscarImoveisDaAPI();
-
-// async function getBuscarImoveisDaAPI() {
-//   const res = await fetch(endpointDaAPI);
-//   imoveis = await res.json();  // Aqui o array "imoveis" é alimentado/atualizado
-//   exibirImoveisNaTela(imoveis)
-// }
-
-// MENU HAMBURGUER - GERENCIAMENTO DO DISPLAY ENTRE BLOCK E NONE
+// MENU HAMBURGUER (GERENCIAMENTO DO DISPLAY ENTRE BLOCK E NONE)
+// Capturqando elementos necessários no DOM
 const imgMenuHamburguer = document.getElementById('burguer');
 const menuListaMenuHamburguer = document.getElementById('itens');
-
 // Comportamento ao clicar na img do menu hambúrguer
 imgMenuHamburguer.addEventListener('click', (evento) => {
   if (evento.target.id == imgMenuHamburguer.id) {
@@ -30,7 +18,3 @@ document.addEventListener('click', (evento) => {
     imgMenuHamburguer.classList.add('cabecalho__img__menu-hamburguer');
   }
 });
-
-// COMPORTAMENTO DOS BOTÕES DE FILTRO DO MENU HAMBÚRGUER
-// const botoesDeFiltro = document.querySelectorAll('.link-de-filtro');
-// botoesDeFiltro.forEach(botaoDeFiltro => botaoDeFiltro.addEventListener('click', filtrarImoveis));
